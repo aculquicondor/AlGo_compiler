b27a80819c049e4e2263dfbbe859d4fb789ddfc2
Aldo Culquicondor <alculquicondor@gmail.com>
2016-06-20 15:42:21 -0500

    rules for function declarations

--- semantic_rules.cpp
+++ semantic_rules.cpp
@@ -2,13 +2,18 @@
 #include <iostream>
 
 
-void declaration(RuleContext &context, bool is_const) {
-    std::string variable = context.get_lexeme(Token::IDENT);
-    if (not context.get_symbol_table().add_symbol(variable)) {
-        throw SemanticError("Redeclaration of \"" + variable + "\"",
+std::string add_ident(RuleContext &context) {
+    std::string name = context.get_lexeme(Token::IDENT);
+    if (not context.get_symbol_table().add_symbol(name)) {
+        throw SemanticError("Redeclaration of \"" + name + "\"",
                             context.get_attributes(Token::IDENT).line_no);
     }
-    auto &record = context.get_symbol_table().get_record(variable);
+    return name;
+}
+
+void declaration(RuleContext &context, bool is_const) {
+    std::string name = add_ident(context);
+    auto &record = context.get_symbol_table().get_record(name);
     record.type_dim = context.get_attributes(SyntaxSymbol::TYPEp).type_dim;
     record.is_const = is_const;
 }
@@ -31,6 +36,14 @@ void copy_back_2(RuleContext &context, SyntaxSymbol to_symbol, SyntaxSymbol from
     context.get_attributes(to_symbol) =
             context.get_attributes(from_symbol);
 }
+
+void forward_add_params(RuleContext &context, SyntaxSymbol symbol, std::size_t r_idx) {
+    add_ident(context);
+    auto &params = context.get_attributes(SyntaxSymbol::PARAM_LISTp).params;
+    params = context.get_attributes(symbol, r_idx).params;
+    params.push_back(context.get_attributes(SyntaxSymbol::TYPEp).type_dim);
+}
+
 
 const std::vector<SemanticRule> semantic_rules = {
         // 0: const declaration
@@ -66,17 +79,49 @@ const std::vector<SemanticRule> semantic_rules = {
         // 14: get int value from decimal
         std::bind(set_int_value, std::placeholders::_1, Token::HEXADEC),
 
-        [](RuleContext &context) { // 15: forward-transmit TYPEp attributes
+        // 15: forward TYPEp attributes
+        [](RuleContext &context) {
             auto &t = context.get_attributes(SyntaxSymbol::TYPEp).type_dim;
             t.dimension = context.get_attributes(SyntaxSymbol::TYPEp, 1).type_dim.dimension;
             t.dimension.push_back((std::size_t)context.get_attributes(SyntaxSymbol::INT_LIT).int_value);
         },
-        // 16: copy back TYPEp attributes
+        // 16: copy-back TYPEp attributes
         std::bind(copy_back, std::placeholders::_1, SyntaxSymbol::TYPEp),
-        // 17: copy back type from TYPE to TYPEp
+        // 17: copy-back attributes from TYPE to TYPEp
         std::bind(copy_back_2, std::placeholders::_1, SyntaxSymbol::TYPEp, SyntaxSymbol::TYPE),
 
         // 18: variable declaration
         std::bind(declaration, std::placeholders::_1, false),
 
+        // 19: copy-back attributes from TYPEp to FUNC_DECLPp
+        std::bind(copy_back_2, std::placeholders::_1, SyntaxSymbol::FUNC_DECLp, SyntaxSymbol::TYPEp),
+
+        // 20: declare function and create new scope
+        [](RuleContext &context) {
+            add_ident(context);
+            context.get_symbol_table().start_scope();
+        },
+        // 21: set function params and return type
+        [](RuleContext &context) {
+            std::string name = add_ident(context);
+            auto &record = context.get_symbol_table().get_record(name);
+            record.type_dim = context.get_attributes(SyntaxSymbol::FUNC_DECLp).type_dim;
+            record.params = context.get_attributes(SyntaxSymbol::PARAM_LIST).params;
+
+            context.get_attributes(SyntaxSymbol::BLOCK).return_type_dim = record.type_dim;
+        },
+
+        // 22: end scope
+        [](RuleContext &context) {
+            context.get_symbol_table().end_scope();
+        },
+
+        // 23: forward & add params from PARAM_LIST
+        std::bind(forward_add_params, std::placeholders::_1, SyntaxSymbol::PARAM_LIST, 0),
+        // 24: forward/copy-back params
+        std::bind(copy_back_2, std::placeholders::_1, SyntaxSymbol::PARAM_LIST, SyntaxSymbol::PARAM_LISTp),
+        // 25: forward & add params from PARAM_LISTp
+        std::bind(forward_add_params, std::placeholders::_1, SyntaxSymbol::PARAM_LISTp, 1),
+        // 26 copy-back params
+        std::bind(copy_back, std::placeholders::_1, SyntaxSymbol::PARAM_LISTp),
 };
